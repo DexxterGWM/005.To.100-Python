fb26bbc5bbdb0ee4e018be0fdb0483b6c4b26b27
DexxterGWM <99261879+DexxterGWM@users.noreply.github.com>
2023-06-16 12:23:17 -0300

    main.py File

---
+++ 03. [Interactive Coding Exercise]/main.py
@@ -0,0 +1,14 @@
+# Write your code below this line 👇🏻
+total = 0
+
+for number in range(2, 101, 2):
+    total += number
+print(total)
+
+total2 = 0
+
+for number in range(1, 101):
+    if number % 2 == 0:
+        total2 += number
+print(total2)
+# Write your code above this line 👆🏻
